356fbade7e9bc5cb79a0dfb9f6d8e8a75b4dd63e
galaxlabs <galaxylab2020@gmail.com>
2025-11-09 20:02:04 +0500

    react use

--- app/map/[slug]/page.tsx
+++ app/map/[slug]/page.tsx
@@ -1,6 +1,16 @@
-// app/map/[slug]/page.tsx
+import { use } from 'react';
 import MapView from '@/app/components/MapView';
 
-export default function Page({ params }: { params: { slug: string } }) {
-  return <MapView slug={params.slug} />;
+export default function Page(
+  { params }: { params: Promise<{ slug: string }> }
+) {
+  const { slug } = use(params);    // ✅ unwrap via React.use()
+  return <MapView slug={slug} />;
 }
+
+// // app/map/[slug]/page.tsx
+// import MapView from '@/app/components/MapView';
+
+// export default function Page({ params }: { params: { slug: string } }) {
+//   return <MapView slug={params.slug} />;
+// }
